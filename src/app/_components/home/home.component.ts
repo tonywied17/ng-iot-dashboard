8272f1c55240ab0a849c183c1d1c7caa412650e9
Tony Wiedman <107732340+tonywied17@users.noreply.github.com>
2023-09-25 17:10:16 -0400

    masonry columns/cards

--- src/app/_components/home/home.component.ts
+++ src/app/_components/home/home.component.ts
@@ -7,4 +7,87 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  cards = [
+    {
+      title: 'Card 1',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      imageUrl: 'https://via.placeholder.com/150x200',
+    },
+    {
+      title: 'Card 2',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus bibendum.',
+      imageUrl: 'https://via.placeholder.com/200x150',
+    },
+    {
+      title: 'Card 3',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque et malesuada ligula.',
+      imageUrl: 'https://via.placeholder.com/250x200',
+    },
+    {
+      title: 'Card 4',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla at eros quis.',
+      imageUrl: 'https://via.placeholder.com/180x220',
+    },
+    {
+      title: 'Card 5',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu lorem sed enim.',
+      imageUrl: 'https://via.placeholder.com/220x180',
+    },
+    {
+      title: 'Card 6',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis vel metus odio.',
+      imageUrl: 'https://via.placeholder.com/200x250',
+    },
+    {
+      title: 'Card 7',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum et odio.',
+      imageUrl: 'https://via.placeholder.com/250x200',
+    },
+    {
+      title: 'Card 8',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce facilisis.',
+      imageUrl: 'https://via.placeholder.com/210x190',
+    },
+    {
+      title: 'Card 9',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tincidunt lacinia.',
+      imageUrl: 'https://via.placeholder.com/190x210',
+    },
+    {
+      title: 'Card 10',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed auctor tincidunt.',
+      imageUrl: 'https://via.placeholder.com/220x180',
+    },
+    {
+      title: 'Card 11',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus elementum.',
+      imageUrl: 'https://via.placeholder.com/180x220',
+    },
+    {
+      title: 'Card 12',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras tempus tincidunt.',
+      imageUrl: 'https://via.placeholder.com/200x250',
+    },
+    {
+      title: 'Card 13',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vehicula ante.',
+      imageUrl: 'https://via.placeholder.com/250x200',
+    },
+    {
+      title: 'Card 14',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque id tellus.',
+      imageUrl: 'https://via.placeholder.com/180x220',
+    },
+    {
+      title: 'Card 15',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam congue justo.',
+      imageUrl: 'https://via.placeholder.com/210x190',
+    },
+    {
+      title: 'Card 16',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tincidunt.',
+      imageUrl: 'https://via.placeholder.com/190x210',
+    },
+  ];
+
 }
